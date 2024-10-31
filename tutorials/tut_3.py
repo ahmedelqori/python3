5f911baef62e0c065e5c924a86b79201158b339d
meedivo <meedivo@gmail.com>
2024-10-31 13:58:51 +0000

    Data type

---
+++ tutorials/tut_3.py
@@ -0,0 +1,22 @@
+# --------------------
+# type(): typeof data
+# All data in Python is object
+# *------------------- 
+
+
+print(10)
+print(type(10)) # int => integer
+
+print(type(100.9)) # float => Floating Point Number
+
+print(type("Meedivo")) # str => String
+
+print(type([1, 2, 3])) # list => List
+
+print(type((1, 2, 3))) # tuple => tuple
+
+print(type({"one":1, "two":2})) # dic = Dictionary
+
+print(type(True)) # bool => Boolean
+
+print(type(None)) # NoneType => Null
