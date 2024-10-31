942bd951fb455a979c139d8d61f8a840cbee0aa2
meedivo <meedivo@gmail.com>
2024-10-31 14:10:44 +0000

    Variables

---
+++ tutorials/tut_4.py
@@ -0,0 +1,23 @@
+# ------------------------
+#       Variables
+# 
+# Syntax => [Variable Name] [Assignment Operator] [Value]
+#
+# Name Convention and rules
+# [1] You Can start (a-z A-Z) or UnderScor
+# [2] You Can't Start With Numbers or Special Characthers
+# [3] can include (0-9) or underscore
+# [4] cannot include Special Characthers
+# [5] Name is Not like name [case Sensitive]
+# ------------------------
+
+myVariable = "My Value"
+
+print(myVariable)
+
+_myVariable = "_My Value"
+
+print(_myVariable)
+
+# -myVairable  NOooooooo
+# 100MyVaribale Noooooooo
