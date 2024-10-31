fad8935e94ad796471887cc818f6fb809dde92e0
meedivo <meedivo@gmail.com>
2024-10-31 13:46:53 +0000

    Syntax

---
+++ tutorials/tut_1.py
@@ -0,0 +1,10 @@
+print("Hello World");
+print("block of code");
+
+if True:
+    print("Python3");
+    print("New Python3");
+
+# if True:
+#    print("Python3");
+#       print("New Python3");  # Not Working
